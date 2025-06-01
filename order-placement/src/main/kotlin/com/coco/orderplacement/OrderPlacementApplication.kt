40e0cf7d77556c2e8e99520f39841de7c6353faf
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2025-06-01 14:59:55 +0900

    재고 차감 이벤트 분리 어플리케이션

---
+++ order-placement/src/main/kotlin/com/coco/orderplacement/OrderPlacementApplication.kt
@@ -0,0 +1,11 @@
+package com.coco.orderplacement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderPlacementApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderPlacementApplication>(*args)
+}
